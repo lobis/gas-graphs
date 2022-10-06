f107dfcdd12c9e6bd13e6df0b3290dc762a2cfb6
Luis Obis <luis.antonio.obis@gmail.com>
2022-10-06 09:55:50 +0200

    update script data

--- scripts/gasFilesList.js
+++ scripts/gasFilesList.js
@@ -6,12 +6,21 @@ const gasFilesJsonDir = path.join(path.basename(__filename), "../public/gas/json
 
 const gasFiles = fs.readdirSync(gasFilesJsonDir).map(function (file) {
     const filepath = path.parse(file)
-    if (filepath.ext !== ".json") {
+
+    try {
+        const data = JSON.parse(fs.readFileSync(path.join(gasFilesJsonDir, file)))
+        return {
+            filename: filepath.base,
+            pressure: data.pressure,
+            temperature: data.temperature,
+            components: data.components
+        }
+    }
+    catch (error) {
+        console.log(`warning: '${file}' is not a valid JSON file`)
         return null
     }
-    return {
-        filename: filepath.base,
-    }
+
 }).filter((item) => item !== null)
 
 const gasFilesListFilename = path.join(path.basename(__filename), "../public/gas/list.json");
